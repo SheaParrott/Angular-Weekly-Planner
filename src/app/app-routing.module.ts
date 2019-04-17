f93b2564a25e0fceb4e710e269c3c1aa70125e81
SheaParrott <alexandergotthis@gmail.com>
2019-04-16 22:58:33 -0400

    routing set up and working. Planned out how more advanced navigation will work as well.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { WeekComponent } from './week/week.component'
+import { AboutComponent } from './about/about.component'
+import { UpComingEventsComponent } from './up-coming-events/up-coming-events.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'planner', component: WeekComponent },
+  { path: 'upcoming', component: UpComingEventsComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core'
 })
 export class NavComponent implements OnInit {
   title = 'Destined'
-  navigation = ['HOME', 'PLANNER', 'UPCOMING', 'ABOUT']
+  navigation = [
+    { name: 'HOME', url: '/', options: [] },
+    { name: 'PLANNER', url: '/planner', options: [] },
+    { name: 'UPCOMING', url: '/upcoming', options: [] },
+    { name: 'ABOUT', url: '/about', options: [] }
+  ]
   constructor() {}
 
   ngOnInit() {}

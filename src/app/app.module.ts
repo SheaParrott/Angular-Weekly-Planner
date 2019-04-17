9c4cfb9e109f8660012ed3c722c5645b2e08597f
SheaParrott <alexandergotthis@gmail.com>
2019-04-16 22:27:28 -0400

    generated pages for project. Now have a visually aesthetic and responsive nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeekComponent } from './week/week.component';
+import { DayComponent } from './day/day.component';
+import { UpComingEventsComponent } from './up-coming-events/up-coming-events.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeekComponent,
+    DayComponent,
+    UpComingEventsComponent,
+    AboutComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/up-coming-events/up-coming-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-up-coming-events',
+  templateUrl: './up-coming-events.component.html',
+  styleUrls: ['./up-coming-events.component.scss']
+})
+export class UpComingEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

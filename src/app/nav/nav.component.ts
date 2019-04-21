df8d426372e1b6e3d87ddef4f87661f9e35d695a
SheaParrott <alexandergotthis@gmail.com>
2019-04-20 23:22:26 -0400

    added extended options for nav bar.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,11 +8,25 @@ import { Component, OnInit } from '@angular/core'
 export class NavComponent implements OnInit {
   title = 'Destined'
   navigation = [
-    { name: 'HOME', url: '/', options: [] },
-    { name: 'PLANNER', url: '/planner', options: [] },
-    { name: 'UPCOMING', url: '/upcoming', options: [] },
+    {
+      name: 'HOME',
+      url: '/',
+      options: [{ name: 'FEATURES', url: '/' }, { name: 'CONTACT', url: '/' }]
+    },
+    {
+      name: 'PLANNER',
+      url: '/planner',
+      options: [
+        { name: 'DAY', url: '/planner' },
+        { name: 'UPCOMING', url: '/upcoming' }
+      ]
+    },
     { name: 'ABOUT', url: '/about', options: [] }
   ]
+  //   <!-- each navigation option have a drop down to take you to different sections of page/ display different options -->
+  // <!-- home: home, features, contact -->
+  // <!-- Planner: week(will have the week and 3 soonest upcoming events with a click to see all), day, upcoming events (include option for linking to google calendar) -->
+  // <!-- about: mission statement, contact, creator -->
   constructor() {}
 
   ngOnInit() {}
